37568f52ac1a004d9f8559b89b1af417c044907c
Pagani Walter <paganiwalter@gmail.com>
2022-07-16 07:55:57 -0300

    Adding firelands prefix to the database

--- data/sql/create/drop_mysql_8.sql
+++ data/sql/create/drop_mysql_8.sql
@@ -2,10 +2,10 @@ REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'firelands'@'%';
 
 DROP USER 'firelands'@'%';
 
-DROP DATABASE IF EXISTS `world`;
+DROP DATABASE IF EXISTS `firelands_world`;
 
-DROP DATABASE IF EXISTS `characters`;
+DROP DATABASE IF EXISTS `firelands_characters`;
 
-DROP DATABASE IF EXISTS `auth`;
+DROP DATABASE IF EXISTS `firelands_auth`;
 
-DROP DATABASE IF EXISTS `hotfixes`;
+DROP DATABASE IF EXISTS `firelands_hotfixes`;
