f3edd9bdb90e15efa4639ba615af6934010cdc27
Pagani Walter <paganiwalter@gmail.com>
2022-07-16 08:04:13 -0300

    default database

--- data/sql/create/drop_mysql_8.sql
+++ data/sql/create/drop_mysql_8.sql
@@ -1,6 +1,6 @@
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'firelands'@'%';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'firelands'@'localhost';
 
-DROP USER 'firelands'@'%';
+DROP USER 'firelands'@'localhost';
 
 DROP DATABASE IF EXISTS `firelands_world`;
 
