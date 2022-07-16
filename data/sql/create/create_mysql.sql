d007d846502b5993fc7a81d54d45580dd7236e2d
Pagani Walter <paganiwalter@gmail.com>
2022-07-16 07:55:57 -0300

    Adding firelands prefix to the database

--- data/sql/create/create_mysql.sql
+++ data/sql/create/create_mysql.sql
@@ -1,19 +1,17 @@
 CREATE USER 'firelands'@'%' IDENTIFIED BY 'uLsZo483XN8b8fqAPb8Z' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0;
 
-GRANT USAGE ON * . * TO 'firelands'@'%';
+CREATE DATABASE `firelands_world` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE DATABASE `world` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `firelands_characters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE DATABASE `characters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `firelands_auth` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE DATABASE `auth` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `firelands_hotfixes` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE DATABASE `hotfixes` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL PRIVILEGES ON `firelands_world` . * TO 'firelands'@'%' WITH GRANT OPTION;
 
-GRANT ALL PRIVILEGES ON `world` . * TO 'firelands'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `firelands_characters` . * TO 'firelands'@'%' WITH GRANT OPTION;
 
-GRANT ALL PRIVILEGES ON `characters` . * TO 'firelands'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `firelands_auth` . * TO 'firelands'@'%' WITH GRANT OPTION;
 
-GRANT ALL PRIVILEGES ON `auth` . * TO 'firelands'@'%' WITH GRANT OPTION;
-
-GRANT ALL PRIVILEGES ON `hotfixes` . * TO 'firelands'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `firelands_hotfixes` . * TO 'firelands'@'%' WITH GRANT OPTION;
--- data/sql/create/drop_mysql.sql
+++ data/sql/create/drop_mysql.sql
@@ -1,27 +1,27 @@
 REVOKE ALL PRIVILEGES ON * . * FROM 'firelands'@'%';
 
-REVOKE ALL PRIVILEGES ON `world` . * FROM 'firelands'@'%';
+REVOKE ALL PRIVILEGES ON `firelands_world` . * FROM 'firelands'@'%';
 
-REVOKE GRANT OPTION ON `world` . * FROM 'firelands'@'%';
+REVOKE GRANT OPTION ON `firelands_world` . * FROM 'firelands'@'%';
 
-REVOKE ALL PRIVILEGES ON `characters` . * FROM 'firelands'@'%';
+REVOKE ALL PRIVILEGES ON `firelands_characters` . * FROM 'firelands'@'%';
 
-REVOKE GRANT OPTION ON `characters` . * FROM 'firelands'@'%';
+REVOKE GRANT OPTION ON `firelands_characters` . * FROM 'firelands'@'%';
 
-REVOKE ALL PRIVILEGES ON `auth` . * FROM 'firelands'@'%';
+REVOKE ALL PRIVILEGES ON `firelands_auth` . * FROM 'firelands'@'%';
 
-REVOKE GRANT OPTION ON `auth` . * FROM 'firelands'@'%';
+REVOKE GRANT OPTION ON `firelands_auth` . * FROM 'firelands'@'%';
 
-REVOKE ALL PRIVILEGES ON `hotfixes` . * FROM 'firelands'@'%';
+REVOKE ALL PRIVILEGES ON `firelands_hotfixes` . * FROM 'firelands'@'%';
 
-REVOKE GRANT OPTION ON `hotfixes` . * FROM 'firelands'@'%';
+REVOKE GRANT OPTION ON `firelands_hotfixes` . * FROM 'firelands'@'%';
 
 DROP USER 'firelands'@'%';
 
-DROP DATABASE IF EXISTS `world`;
+DROP DATABASE IF EXISTS `firelands_world`;
 
-DROP DATABASE IF EXISTS `characters`;
+DROP DATABASE IF EXISTS `firelands_characters`;
 
-DROP DATABASE IF EXISTS `auth`;
+DROP DATABASE IF EXISTS `firelands_auth`;
 
-DROP DATABASE IF EXISTS `hotfixes`;
+DROP DATABASE IF EXISTS `firelands_hotfixes`;
